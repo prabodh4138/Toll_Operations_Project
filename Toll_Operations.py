0642b1fe4cca66072ff85e339bd54f009cbfefe2
prabodh4138 <prabodh4138@gmail.com>
2025-07-14 20:00:36 +0530

    Update Toll_Operations.py

--- Toll_Operations.py
+++ Toll_Operations.py
@@ -9,16 +9,24 @@ def main():
         [
             "DG Monitoring",
             "EB Meter Reading",
-            # "Highway Reading",
-            # "Inventory Management"
+            "Highway Reading",
+            "Inventory Management (Coming Soon)"
         ]
     )
 
     if module == "DG Monitoring":
         diesel_monitoring_app.run()
+
     elif module == "EB Meter Reading":
         import eb_meter_reading_app
         eb_meter_reading_app.run()
+
+    elif module == "Highway Reading":
+        import highway_reading_app
+        highway_reading_app.run()
+
+    elif module == "Inventory Management (Coming Soon)":
+        st.info("🛠️ Inventory Management module is under development and will be available soon.")
 
 if __name__ == "__main__":
     main()
